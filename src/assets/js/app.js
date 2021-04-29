aec7205811904d442a6356da886ae9ff776a78b4
Alexandr <yaborodinov@gmail.com>
2021-04-29 17:29:34 +0300

    finished

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,14 @@
-console.log('file 1');
+
+
+const burgerBtn = document.querySelector(".burger")
+const mobileMenu = document.querySelector('.header__left .nav');
+const header = document.querySelector(".header");
+const main = document.querySelector(".main");
+
+
+
+burgerBtn.addEventListener("click", () => {
+    burgerBtn.classList.toggle("active");
+    mobileMenu.classList.toggle("active");
+    main.classList.toggle("disable")
+})
